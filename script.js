4ce4ca130256172d7a7c310038e5031524119624
Thomas Steiner <tomac@google.com>
2021-08-27 09:33:08 +0200

    Change path

--- script.js
+++ script.js
@@ -10,11 +10,11 @@ link.addEventListener('input', () => {
 
 reset.addEventListener('click', async () => {
   img.src =
-    'https://cdn.glitch.com/9550a029-cbe2-4e54-bba1-0bd6b35b3c89%2Fdda943b2-2206-45c7-8765-b8c0d2464edd-large.png?v=1629884729569';
+    './avatar.png';
   name.textContent = 'Thomas Steiner';
   role.textContent = 'Developer Relations Engineer at Google.';
   link.textContent = '@tomayac';
-  link.href = 'https://twitter.com/tomayac';  
+  link.href = 'https://twitter.com/tomayac';
 });
 
 if ('serviceWorker' in navigator) {
